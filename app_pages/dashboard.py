f1eaad2b11e2251af1eacebd76c0ddfc2e4506dd
elsei123 <eltonseidi@gmail.com>
2025-03-08 04:49:37 +0000

    Add imports and system path configuration

---
+++ app_pages/dashboard.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from src.utils import get_sample_data
+from src.model import load_trained_model
+from tensorflow.keras.models import load_model
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
